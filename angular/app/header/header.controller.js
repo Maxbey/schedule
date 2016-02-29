e62574230af0523ac47966bac8080096de5308c9
alekseylymarev <aleseylymarev@gmail.com>
2016-03-01 00:28:56 +0600

    was added sidebar to template

--- angular/app/header/header.controller.js
+++ angular/app/header/header.controller.js
@@ -3,8 +3,13 @@
 
     angular.module('app.controllers').controller('HeaderController', HeaderController);
 
-    function HeaderController(){
-      
+    function HeaderController($scope, $mdSidenav){
+      var vm = this;
+      vm.sidebar_is_opened = false;
+
+      vm.toggle = function(){
+        vm.sidebar_is_opened = !vm.sidebar_is_opened;
+      }
     }
 
 })();
